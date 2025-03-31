b07ac575b81bd7404c5e70af1332d81bac0866bc
twisted_pear <twstd@posteo.net>
2025-04-01 00:16:51 +0200

    Show ethtool errors in install script

--- install.sh
+++ install.sh
@@ -14,15 +14,15 @@ IF="${1}"
 
 IPX_WRAP_DIR="`dirname ${0}`"
 
-ethtool -K "${IF}" tx off >/dev/null 2>&1
-ethtool -K "${IF}" rx off >/dev/null 2>&1
-ethtool -K "${IF}" generic-segmentation-offload off >/dev/null 2>&1
-ethtool -K "${IF}" scatter-gather off >/dev/null 2>&1
-ethtool -K "${IF}" tx-gso-list off >/dev/null 2>&1
-ethtool -K "${IF}" tx-ipxip4-segmentation off >/dev/null 2>&1
-ethtool -K "${IF}" tx-ipxip6-segmentation off >/dev/null 2>&1
-ethtool -K "${IF}" tx-udp_tnl-segmentation off >/dev/null 2>&1
-ethtool -K "${IF}" tx-udp_tnl-csum-segmentation off >/dev/null 2>&1
+ethtool -K "${IF}" tx off >/dev/null
+ethtool -K "${IF}" rx off >/dev/null
+ethtool -K "${IF}" generic-segmentation-offload off >/dev/null
+ethtool -K "${IF}" scatter-gather off >/dev/null
+ethtool -K "${IF}" tx-gso-list off >/dev/null
+ethtool -K "${IF}" tx-ipxip4-segmentation off >/dev/null
+ethtool -K "${IF}" tx-ipxip6-segmentation off >/dev/null
+ethtool -K "${IF}" tx-udp_tnl-segmentation off >/dev/null
+ethtool -K "${IF}" tx-udp_tnl-csum-segmentation off >/dev/null
 
 tc qdisc add dev "${IF}" clsact
 tc filter add dev "${IF}" ingress bpf object-file "${IPX_WRAP_DIR}/ipx_wrap_kern.o" section tc/ingress direct-action
